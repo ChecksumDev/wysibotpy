d37cfa8b7281a34f50733e340430961db65d73a0
Checksum <checksum@velvet.moe>
2022-12-29 15:16:09 -0600

    refactor: clarify

--- src/modules/bot.py
+++ src/modules/bot.py
@@ -103,7 +103,7 @@ class Client:
                     try:
                         clip_resp = await self.twitch.create_clip(user.id)
                         clip_link = f"https://clips.twitch.tv/{clip_resp.id}"
-                    except:
+                    except Exception:
                         logger.warning(
                             f"Tried to create a clip for {user.display_name} but was unable too.")
 
