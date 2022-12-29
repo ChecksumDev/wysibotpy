8e80ca2f389b68ef96870165228b2b7f8befca7b
Checksum <checksum@velvet.moe>
2022-12-29 15:23:35 -0600

    fix: event loop not working serverside

--- src/main.py
+++ src/main.py
@@ -7,13 +7,14 @@ from utilities.config import get_config
 async def main():
     await client.start()
 
-
 if __name__ == "__main__":
     config = get_config()
     client = Client(config)
 
     try:
         loop = asyncio.get_event_loop()
-        loop.run_until_complete(main())
+        task = loop.create_task(main())
+        task.add_done_callback(lambda _: asyncio.run(client.shutdown()))
+        loop.run_until_complete(task)
     except (KeyboardInterrupt):
         asyncio.run(client.shutdown())
